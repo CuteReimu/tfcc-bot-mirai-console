b386c18802ba27dc8c4f487958ba8a3b9167e2c8
CuteReimu <415551921@qq.com>
2023-03-09 12:02:31 +0800

    优化文件结构

--- src/main/kotlin/CommandHandler.kt
+++ src/main/kotlin/CommandHandler.kt
@@ -1,9 +1,15 @@
 package org.tfcc.bot
 
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import net.mamoe.mirai.event.events.GroupMessageEvent
+import net.mamoe.mirai.message.data.At
 import net.mamoe.mirai.message.data.MessageChain
+import net.mamoe.mirai.message.data.PlainText
 import org.tfcc.bot.command.*
+import org.tfcc.bot.storage.TFCCConfig
 
 /**
  * 这是聊天指令处理器的接口，当你想要新增自己的聊天指令处理器时，实现这个接口即可。
@@ -40,5 +46,37 @@ interface CommandHandler {
             GetLiveState, StartLive, StopLive, ChangeLiveTitle,
             RandGame,
         )
+
+        @OptIn(DelicateCoroutinesApi::class)
+        fun handle(e: GroupMessageEvent) {
+            if (e.group.id !in TFCCConfig.qq.qqGroup)
+                return
+            val message = e.message
+            GlobalScope.launch {
+                if (message.size <= 1)
+                    return@launch
+                val isAt = message.getOrNull(1)?.let { it is At } ?: false
+                if (!isAt && message.size > 2 || message.size > 3)
+                    return@launch
+                val msg =
+                    if (isAt) (message.getOrNull(2) as? PlainText)?.content ?: ShowTips.name
+                    else (message.getOrNull(1) as? PlainText)?.content ?: return@launch
+                val msgContent = msg.trim()
+                if (msgContent.contains("\n") || msgContent.contains("\r"))
+                    return@launch
+                val msgSlices = msgContent.split(" ", limit = 2)
+                val cmd = msgSlices[0]
+                val content = msgSlices.getOrElse(1) { "" }
+                handlers.forEach {
+                    if (it.name == cmd && it.checkAuth(e.group.id, e.sender.id)) {
+                        val (groupMsg, privateMsg) = it.execute(e, content)
+                        if (groupMsg != null)
+                            e.group.sendMessage(groupMsg)
+                        if (privateMsg != null)
+                            e.sender.sendMessage(privateMsg)
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -1,20 +1,18 @@
 package org.tfcc.bot
 
 import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.launch
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import net.mamoe.mirai.event.Event
 import net.mamoe.mirai.event.EventPriority
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.globalEventChannel
-import net.mamoe.mirai.message.data.At
-import net.mamoe.mirai.message.data.PlainText
 import org.tfcc.bot.bilibili.Bilibili
-import org.tfcc.bot.command.ShowTips
 import org.tfcc.bot.storage.BilibiliData
 import org.tfcc.bot.storage.PermData
 import org.tfcc.bot.storage.TFCCConfig
+import kotlin.reflect.KClass
 
 @ConsoleExperimentalApi
 internal object PluginMain : KotlinPlugin(
@@ -29,45 +27,18 @@ internal object PluginMain : KotlinPlugin(
         TFCCConfig.reload()
         PermData.reload()
         Bilibili.init()
-        initCommandHandler()
+        initHandler(GroupMessageEvent::class, CommandHandler::handle)
     }
 
-    private fun initCommandHandler() {
+    private fun <E : Event> initHandler(eventClass: KClass<out E>, handler: (E) -> Unit) {
         globalEventChannel().subscribeAlways(
-            GroupMessageEvent::class,
+            eventClass,
             CoroutineExceptionHandler { _, throwable ->
                 logger.error(throwable)
             },
             priority = EventPriority.MONITOR,
-        ) call@{
-            if (group.id !in TFCCConfig.qq.qqGroup)
-                return@call
-
-            launch {
-                if (message.size <= 1)
-                    return@launch
-                val isAt = message.getOrNull(1)?.let { it is At } ?: false
-                if (!isAt && message.size > 2 || message.size > 3)
-                    return@launch
-                val msg =
-                    if (isAt) (message.getOrNull(2) as? PlainText)?.content ?: ShowTips.name
-                    else (message.getOrNull(1) as? PlainText)?.content ?: return@launch
-                val msgContent = msg.trim()
-                if (msgContent.contains("\n") || msgContent.contains("\r"))
-                    return@launch
-                val msgSlices = msgContent.split(" ", limit = 2)
-                val cmd = msgSlices[0]
-                val content = msgSlices.getOrElse(1) { "" }
-                CommandHandler.handlers.forEach {
-                    if (it.name == cmd && it.checkAuth(this@call.group.id, this@call.sender.id)) {
-                        val (groupMsg, privateMsg) = it.execute(this@call, content)
-                        if (groupMsg != null)
-                            this@call.group.sendMessage(groupMsg)
-                        if (privateMsg != null)
-                            this@call.sender.sendMessage(privateMsg)
-                    }
-                }
-            }
+        ) {
+            handler(this)
         }
     }
 }
