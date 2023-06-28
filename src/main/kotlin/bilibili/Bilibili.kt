2d2d89cdb09bc00a59cb12755bcdcb43a71b6ccb
CuteReimu <415551921@qq.com>
2023-06-28 17:47:39 +0800

    优化代码

--- src/main/kotlin/bilibili/Bilibili.kt
+++ src/main/kotlin/bilibili/Bilibili.kt
@@ -102,7 +102,7 @@ object Bilibili {
 
     private const val ua =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.69"
-    private var client = OkHttpClient().newBuilder().followRedirects(false)
+    private val client = OkHttpClient().newBuilder().followRedirects(false)
         .connectTimeout(Duration.ofMillis(20000)).cookieJar(CookieJarWithData).build()
 
     fun init() {
